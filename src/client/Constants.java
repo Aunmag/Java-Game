61088671e8219e9b34f9da748ec5fece4e318afa
Aunmag <aunmag@gmail.com>
2017-06-25 22:18:58 +0500

    Reset version and isDebug

--- src/client/Constants.java
+++ src/client/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 
     public static final String TITLE = "A Zombie Shooter Game";
     public static final String DEVELOPER = "Aunmag";
-    public static final String VERSION = "0.3";
+    public static final String VERSION = "0.3.0";
 
     public static final Font FONT = new Font("Arial", Font.PLAIN, 16);
 
