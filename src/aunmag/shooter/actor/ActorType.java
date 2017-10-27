1b661321549e6bbb1346a75f4b4218d04426765e
Aunmag <aunmag@gmail.com>
2017-10-26 17:50:35 -0700

    Corrected actor strength and melee damage (#43)

--- src/aunmag/shooter/actor/ActorType.java
+++ src/aunmag/shooter/actor/ActorType.java
@@ -4,51 +4,60 @@ import aunmag.nightingale.structures.Texture;
 
 public class ActorType {
 
+    private static final float strengthDefault = 37.5f;
+
     public final Texture texture;
     public final float strength;
     public final float velocity;
     public final float velocityFactorSprint;
+    public final float damage;
 
     private ActorType(
-            Texture texture,
+            String name,
             float strength,
             float velocity,
-            float velocityFactorSprint
+            float velocityFactorSprint,
+            float damage
     ) {
+        this.texture = Texture.getOrCreateAsSprite("images/actors/" + name);
         this.strength = strength;
-        this.texture = texture;
         this.velocity = velocity;
         this.velocityFactorSprint = velocityFactorSprint;
+        this.damage = damage;
     }
 
     /* Types */
 
     public static final ActorType human = new ActorType(
-            Texture.getOrCreateAsSprite("images/actors/human"),
-            37.5f,
+            "human",
+            strengthDefault,
             0.043f,
-            2.76f
+            2.76f,
+            strengthDefault / 16f
     );
 
     public static final ActorType zombieEasy = new ActorType(
-            Texture.getOrCreateAsSprite("images/actors/zombie"),
+            "zombie",
             0.4f * human.strength,
             0.4f * human.velocity,
-            0.4f * human.velocityFactorSprint
+            0.4f * human.velocityFactorSprint,
+            human.strength / 8f
     );
 
     public static final ActorType zombieMedium = new ActorType(
-            Texture.getOrCreateAsSprite("images/actors/zombie"),
+            "zombie",
             0.6f * human.strength,
             0.6f * human.velocity,
-            0.6f * human.velocityFactorSprint
+            0.6f * human.velocityFactorSprint,
+            human.strength / 6f
     );
 
     public static final ActorType zombieHard = new ActorType(
-            Texture.getOrCreateAsSprite("images/actors/zombie"),
+            "zombie",
             0.8f * human.strength,
             0.8f * human.velocity,
-            0.8f * human.velocityFactorSprint
+            0.8f * human.velocityFactorSprint,
+            human.strength / 4f
     );
 
 }
