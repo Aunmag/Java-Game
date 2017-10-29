1fc3921c638507ef99331fa234b63a58cbe214fc
Aunmag <aunmag@gmail.com>
2017-10-28 23:01:29 -0700

    Implemented weapon types (#57)

--- src/aunmag/shooter/weapon/Weapon.java
+++ src/aunmag/shooter/weapon/Weapon.java
@@ -1,43 +1,32 @@
 package aunmag.shooter.weapon;
 
 import aunmag.nightingale.audio.AudioSource;
-import aunmag.nightingale.utilities.UtilsAudio;
 import aunmag.shooter.world.World;
 import aunmag.nightingale.basics.BaseSprite;
-import aunmag.nightingale.structures.Texture;
 import aunmag.nightingale.utilities.UtilsMath;
 
 public class Weapon extends BaseSprite {
 
-    private static final Texture texture = Texture.getOrCreateAsSprite("images/weapons/mp_27");
-
-    public final float velocity;
-    public final float velocityDeflectionFactor;
-    public final float radiansDeflection;
-    public final float recoilRadians;
+    public final WeaponType type;
     public final Magazine magazine;
     public final Striker striker;
     public final Trigger trigger;
     private AudioSource audioSource;
 
     public Weapon(
-            float velocity,
-            float velocityDeflectionFactor,
-            float radiansDeflection,
-            float recoilRadians,
+            WeaponType type,
             Magazine magazine,
             Striker striker,
             Trigger trigger
     ) {
-        super(0, 0, 0, texture);
-        this.velocity = velocity;
-        this.velocityDeflectionFactor = velocityDeflectionFactor;
-        this.radiansDeflection = radiansDeflection;
-        this.recoilRadians = recoilRadians;
+        super(0, 0, 0, type.texture);
+        this.type = type;
         this.magazine = magazine;
         this.striker = striker;
         this.trigger = trigger;
-        audioSource = UtilsAudio.getOrCreateSoundOgg("sounds/weapons/shot_mp_27");
+
+        audioSource = new AudioSource();
+        audioSource.setSample(type.sample);
     }
 
     public void update() {
@@ -49,12 +38,11 @@ public class Weapon extends BaseSprite {
     }
 
     private void makeShot() {
-        float push = UtilsMath.randomizeFlexibly(recoilRadians, recoilRadians * 0.25f);
-        trigger.getShooter().push(UtilsMath.random.nextBoolean() ? push : -push);
+        trigger.getShooter().push(calculateRandomRecoil());
 
         audioSource.play();
 
-        float muzzleLength = texture.getCenterX();
+        float muzzleLength = type.texture.getCenterX();
         float bulletX = getX() + muzzleLength * (float) Math.cos(getRadians());
         float bulletY = getY() + muzzleLength * (float) Math.sin(getRadians());
 
@@ -64,20 +52,33 @@ public class Weapon extends BaseSprite {
     }
 
     private void makeBullet(float x, float y) {
-        float radians = UtilsMath.randomizeFlexibly(getRadians(), radiansDeflection);
-        float velocity = UtilsMath.randomizeFlexibly(
-                this.velocity,
-                this.velocity * velocityDeflectionFactor
-        );
         Projectile projectile = new Projectile(
                 magazine.cartridgeType.projectile,
                 x,
                 y,
-                radians,
-                velocity,
+                calculateRandomRadians(),
+                calculateRandomVelocity(),
                 trigger.getShooter()
         );
         World.projectiles.add(projectile);
+    }
+
+    private float calculateRandomRecoil() {
+        float recoil = UtilsMath.randomizeFlexibly(type.recoil, type.recoilDeflection);
+
+        if (UtilsMath.random.nextBoolean()) {
+            recoil = -recoil;
+        }
+
+        return recoil;
+    }
+
+    private float calculateRandomRadians() {
+        return UtilsMath.randomizeFlexibly(getRadians(), type.radiansDeflection);
+    }
+
+    private float calculateRandomVelocity() {
+        return UtilsMath.randomizeFlexibly(type.velocity, type.velocityDeflection);
     }
 
     /* Setters */
--- src/aunmag/shooter/weapon/WeaponFactory.java
+++ src/aunmag/shooter/weapon/WeaponFactory.java
@@ -3,14 +3,10 @@ package aunmag.shooter.weapon;
 public class WeaponFactory {
 
     private static final int SEMI_AUTO_SHOTS_PER_MINUTE = 400;
-    private static final float VELOCITY_DEFLECTION_FACTOR = 0.03f;
 
     public static Weapon laserGun() {
         return new Weapon(
-                5000,
-                0f,
-                0f,
-                0f,
+                WeaponType.laserGun,
                 new Magazine(CartridgeType.LASER, true, 0, 0),
                 new Striker(1200),
                 new Trigger(true)
@@ -19,10 +15,7 @@ public class WeaponFactory {
 
     public static Weapon makarovPistol() {
         return new Weapon(
-                315,
-                VELOCITY_DEFLECTION_FACTOR,
-                0.05f,
-                0.01f,
+                WeaponType.makarovPistol,
                 new Magazine(CartridgeType._9x18mm_Makarov, true, 8, 2000),
                 new Striker(SEMI_AUTO_SHOTS_PER_MINUTE),
                 new Trigger(false)
@@ -31,10 +24,7 @@ public class WeaponFactory {
 
     public static Weapon mp27() {
         return new Weapon(
-                410,
-                VELOCITY_DEFLECTION_FACTOR,
-                0.06f,
-                0.06f,
+                WeaponType.mp27,
                 new Magazine(CartridgeType._12_76_Magnum, false, 2, 250),
                 new Striker(SEMI_AUTO_SHOTS_PER_MINUTE),
                 new Trigger(false)
@@ -43,10 +33,7 @@ public class WeaponFactory {
 
     public static Weapon aks74u() {
         return new Weapon(
-                735,
-                VELOCITY_DEFLECTION_FACTOR,
-                0.03f,
-                0.02f,
+                WeaponType.aks74u,
                 new Magazine(CartridgeType._5_45x39mm, true, 30, 2000),
                 new Striker(675),
                 new Trigger(true)
@@ -55,10 +42,7 @@ public class WeaponFactory {
 
     public static Weapon pecheneg() {
         return new Weapon(
-                825,
-                VELOCITY_DEFLECTION_FACTOR,
-                0.02f,
-                0.035f,
+                WeaponType.pecheneg,
                 new Magazine(CartridgeType._7_62x54mmR, true, 200, 8000),
                 new Striker(650),
                 new Trigger(true)
@@ -67,10 +51,7 @@ public class WeaponFactory {
 
     public static Weapon saiga12k() {
         return new Weapon(
-                410,
-                VELOCITY_DEFLECTION_FACTOR,
-                0.07f,
-                0.05f,
+                WeaponType.saiga12k,
                 new Magazine(CartridgeType._12_76_Magnum, true, 8, 2000),
                 new Striker(SEMI_AUTO_SHOTS_PER_MINUTE),
                 new Trigger(false)
