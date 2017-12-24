b41d27aa04238bd40e8d7862da3dfb5cba5d60fe
Aunmag <aunmag@gmail.com>
2017-12-24 18:54:39 +0500

    Implemented distinct player class (#72)

--- src/main/java/aunmag/shooter/world/World.kt
+++ src/main/java/aunmag/shooter/world/World.kt
@@ -4,16 +4,13 @@ import aunmag.nightingale.Application
 import aunmag.nightingale.utilities.TimeFlow
 import aunmag.nightingale.utilities.UtilsBaseOperative
 import aunmag.nightingale.utilities.UtilsGraphics
-import aunmag.nightingale.utilities.UtilsMath
 import aunmag.shooter.actor.Actor
-import aunmag.shooter.actor.ActorType
 import aunmag.shooter.ai.Ai
 import aunmag.shooter.client.graphics.WorldGrid
 import aunmag.shooter.data.soundAmbiance
 import aunmag.shooter.data.soundAtmosphere
 import aunmag.shooter.gui.NotificationLayer
 import aunmag.shooter.weapon.Projectile
-import aunmag.shooter.weapon.WeaponFactory
 import org.lwjgl.opengl.GL11
 import java.util.ArrayList
 
@@ -24,29 +21,7 @@ class World {
     val ais: MutableList<Ai> = ArrayList()
     val actors: MutableList<Actor> = ArrayList()
     val projectiles: MutableList<Projectile> = ArrayList()
-    val notifications: NotificationLayer
-
-    // TODO: World shouldn't know about player's weapons
-    val laserGun = WeaponFactory.laserGun(this)
-    val makarovPistol = WeaponFactory.makarovPistol(this)
-    val mp27 = WeaponFactory.mp27(this)
-    val aks74u = WeaponFactory.aks74u(this)
-    val pecheneg = WeaponFactory.pecheneg(this)
-    val saiga12k = WeaponFactory.saiga12k(this)
-
-    init {
-        initializePlayer() // TODO: World should not know about client's player
-        notifications = NotificationLayer(time)
-    }
-
-    private fun initializePlayer() {
-        val player = Actor(ActorType.human, this)
-        player.radians = -UtilsMath.PIx0_5.toFloat()
-        player.weapon = mp27
-
-        Actor.setPlayer(player)
-        actors.add(player)
-    }
+    val notifications = NotificationLayer(time)
 
     fun update() {
         time.add(Application.time.delta, true)
