7f42ddb40e75e067486cbf1a7d1412561a4824d7
Aunmag <aunmag@gmail.com>
2018-01-03 00:09:48 +0500

    Changed menu background

---
+++ src/main/java/aunmag/shooter/scenarios/ScenarioPause.kt
@@ -0,0 +1,30 @@
+package aunmag.shooter.scenarios
+
+import aunmag.nightingale.Application
+import aunmag.nightingale.utilities.UtilsMath
+import aunmag.shooter.client.App
+
+class ScenarioPause {
+
+    private var direction = 0f
+    private val velocity = 0.1f
+
+    init {
+        refresh()
+    }
+
+    fun refresh() {
+        val camera = Application.getCamera()
+        camera.setPosition(0f, 0f)
+        camera.radians = UtilsMath.randomizeBetween(0f, UtilsMath.PIx2.toFloat())
+        direction = UtilsMath.randomizeBetween(0f, UtilsMath.PIx2.toFloat())
+    }
+
+    fun update() {
+        val camera = Application.getCamera()
+        val velocity = this.velocity * App.main.pause.world.time.delta.toFloat()
+        camera.x += velocity * Math.cos(direction.toDouble()).toFloat()
+        camera.y += velocity * Math.sin(direction.toDouble()).toFloat()
+    }
+
+}
