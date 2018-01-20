d2fa0ba3812557be3794c5abeb9a2a0de4470d19
Aunmag <aunmag@gmail.com>
2018-01-20 14:17:32 +0500

    No static class

--- src/main/java/aunmag/shooter/client/graphics/Blackout.java
+++ src/main/java/aunmag/shooter/client/graphics/Blackout.java
@@ -6,36 +6,28 @@ import aunmag.nightingale.utilities.FluidValue;
 import aunmag.nightingale.utilities.UtilsGraphics;
 import aunmag.nightingale.utilities.UtilsMath;
 import aunmag.shooter.actor.Actor;
-import aunmag.shooter.utils.LinksKt;
 import org.lwjgl.opengl.GL11;
 
 public class Blackout {
 
-    private static Texture texture;
-    private static final float healthMax = 1;
-    private static final float healthThird = healthMax / 3f;
-    private static float healthCurrent = healthMax;
-    private static float healthLast = healthCurrent;
-    private static final float timeHurtAscent = 0.06f;
-    private static final float timeHurtDecline = 0.48f;
-    private static FluidValue intensity;
+    private final Actor player;
+    private final Texture texture;
+    private final float healthMax = 1;
+    private final float healthThird = healthMax / 3f;
+    private float healthLast = healthMax;
+    private final float timeHurtAscent = 0.06f;
+    private final float timeHurtDecline = 0.48f;
+    private final FluidValue intensity;
 
-    static {
+    public Blackout(Actor player) {
+        this.player = player;
+
         texture = Texture.getOrCreate("images/gui/blackout1600", false, false);
         texture.scaleAsWindow();
-
-         intensity = new FluidValue(Application.time, timeHurtAscent); // TODO: Use world time
+        intensity = new FluidValue(player.world.getTime(), timeHurtAscent);
     }
 
-    public static void render() {
-        Actor actor = LinksKt.getPlayer();
-
-        if (actor == null) {
-            return;
-        }
-
-        healthCurrent = actor.getHealth();
-
+    public void render() {
         renderBoundaries();
 
         UtilsGraphics.drawPrepare();
@@ -43,8 +35,8 @@ public class Blackout {
         renderDynamicRectangle();
     }
 
-    private static void renderBoundaries() {
-        float alpha = 1 - healthCurrent / 1.4f;
+    private void renderBoundaries() {
+        float alpha = 1 - player.getHealth() / 1.4f;
 
         texture.bind();
         Application.getShader().setUniformProjection(Application.getWindow().projection);
@@ -52,11 +44,11 @@ public class Blackout {
         texture.render();
     }
 
-    private static void renderRectangle() {
+    private void renderRectangle() {
         float healthDoubleThird = healthThird * 2;
 
-        if (healthCurrent <= healthDoubleThird) {
-            float alpha = (healthMax - healthCurrent / healthDoubleThird) * 0.9f;
+        if (player.getHealth() <= healthDoubleThird) {
+            float alpha = (healthMax - player.getHealth() / healthDoubleThird) * 0.9f;
             float width = Application.getWindow().getWidth();
             float height = Application.getWindow().getHeight();
             GL11.glColor4f(0f, 0f, 0f, alpha);
@@ -64,16 +56,16 @@ public class Blackout {
         }
     }
 
-    private static void renderDynamicRectangle() {
+    private void renderDynamicRectangle() {
         intensity.update();
-        if (healthCurrent != healthLast) {
+        if (player.getHealth() != healthLast) {
             if (intensity.timer.getDuration() != timeHurtAscent) {
                 intensity.timer.setDuration(timeHurtAscent);
             }
-            float intensityTarget = Math.abs(healthLast - healthCurrent) * 4;
+            float intensityTarget = Math.abs(healthLast - player.getHealth()) * 4;
             intensity.setTarget(UtilsMath.limitNumber(intensityTarget, 0, 1));
             if (intensity.isTargetReached()) {
-                healthLast = healthCurrent;
+                healthLast = player.getHealth();
                 intensity.timer.setDuration(timeHurtDecline);
             }
         } else {
