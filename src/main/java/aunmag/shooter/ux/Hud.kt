a4c2a2fee972d951e2486e9763e80623ae4eb15f
Aunmag <aunmag@gmail.com>
2018-02-04 06:37:16 +0500

    Added health HUD indicator (#84)

---
+++ src/main/java/aunmag/shooter/ux/Hud.kt
@@ -0,0 +1,69 @@
+package aunmag.shooter.ux
+
+import aunmag.nightingale.font.FontStyleDefault
+import aunmag.nightingale.font.Text
+import aunmag.nightingale.input.Input
+import aunmag.nightingale.utilities.UtilsMath
+import aunmag.shooter.client.App
+import aunmag.shooter.data.player
+import aunmag.shooter.gui.Parameter
+import org.lwjgl.glfw.GLFW
+
+class Hud {
+
+    private val health = Parameter("Health", 0.0f, 30, 33)
+    private val ammo = Parameter("Ammo", 0.0f, 30, 34)
+    private val debug = Text(10f, 10f, "", FontStyleDefault.simple)
+    private var isDebug = false
+
+    fun update() {
+        Parameter.update()
+        health.value = player?.health ?: 0f
+        ammo.value = player?.weapon?.magazine?.calculateVolumeRatio() ?: 0f
+        ammo.isPulsing = player?.weapon?.magazine?.isReloading ?: false
+
+        if (Input.keyboard.isKeyPressed(GLFW.GLFW_KEY_F1)) {
+            isDebug = !isDebug
+        }
+    }
+
+    fun render() {
+        health.render()
+        ammo.render()
+
+        if (isDebug) {
+            renderDebug()
+        }
+    }
+
+    private fun renderDebug() {
+        val game = App.main.game ?: return
+        val world = game.world
+
+        var timeSpentUpdate = 0f // TODO: Invoke data
+        var timeSpentRender = 0f // TODO: Invoke data
+        var timeSpentTotal = timeSpentUpdate + timeSpentRender
+        val round = 100f
+        timeSpentUpdate = UtilsMath.calculateRoundValue(timeSpentUpdate, round)
+        timeSpentRender = UtilsMath.calculateRoundValue(timeSpentRender, round)
+        timeSpentTotal = UtilsMath.calculateRoundValue(timeSpentTotal, round)
+
+        var message = ""
+        message += String.format("Spent time on updating: %s ms\n", timeSpentUpdate)
+        message += String.format("Spent time on rendering: %s ms\n", timeSpentRender)
+        message += String.format("Spent time total: %s ms \n", timeSpentTotal)
+        message += String.format("\nAIs: %s", world.ais.size)
+        message += String.format("\nActors: %s", world.actors.size)
+        message += String.format("\nBullets: %s", world.projectiles.size)
+
+        debug.load(message)
+        debug.orderRendering()
+    }
+
+    fun remove() {
+        health.remove()
+        ammo.remove()
+        debug.remove()
+    }
+
+}
